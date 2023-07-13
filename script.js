2f075e2a73624795073e968347ef1988a64a95cb
Roger Medellin <medr33@yahoo.com>
2023-07-13 16:26:09 -0400

    delete api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var apikey = "32ee970c087eab3aeed6412f800ca5a1";
+var apikey = "3";
 var searchBtn =(".searchBtn");
 
 fetch ("api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}")
