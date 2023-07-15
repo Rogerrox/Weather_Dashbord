d0abd49bbb4e870caa1b317b717a3dacdbb21223
Roger Medellin <medr33@yahoo.com>
2023-07-15 10:33:47 -0400

    added read me

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Weather_Dashbord
+## Weather_Dashbord
+
+# Description
+
+This is a weather report dashboard that shows the current weather conditions for a selected city for that hour. It breaks 
+down the current temp, windspeed, humidity and icon depicting the weather condition. In addition, it will show
+the condition of that city's weather forcast for the next  five days for the same hour of the day. 
+
+# Technologies Used
+
+HTML 
+Bootstrap
+JAVASCRIPT
+JQUERY
+
+# License
+
+MIT License
+
+# Credit 
+
+Tutor Help: Sangeetha Kaliaperumal
+
+# Image of Weather Dashboard
+
+![Alt text](<127.0.0.1_5500_index.html (1).png>)
+
+
+# Questions
+
+Please feel free to contact me at medr33@yahoo.com
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var apiKey = "32ee970c087eab3aeed6412f800ca5a1";
 var searchBtn = document.getElementById("search")
 
 
-searchBtn.addEventListener("click",function(event){
+searchBtn.addEventListener("click", function (event) {
     event.preventDefault()
     var city = document.getElementById("city-input").value
     console.log(city)
@@ -18,13 +18,13 @@ searchBtn.addEventListener("click",function(event){
 
 
 
-function getCurrentWeather(city){
+function getCurrentWeather(city) {
     var URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
     fetch(URL)
-    .then(response => response.json())
-    .then(apiResponse => {
-        console.log(apiResponse)
-        const HTML = `<div class="card" style="width: 18rem;">
+        .then(response => response.json())
+        .then(apiResponse => {
+            console.log(apiResponse)
+            const HTML = `<div class="card" style="width: 18rem;">
         <div class="card-body">
         <h5 class="card-title">${apiResponse.name}</h5>
         <img src="https://openweathermap.org/img/wn/${apiResponse.weather[0].icon}@2x.png" class="card-img-top" alt="...">
@@ -34,20 +34,20 @@ function getCurrentWeather(city){
           <p class="card-text">Description: ${apiResponse.weather[0].main}</p>
                 </div>
       </div>`
-      document.getElementById("current").innerHTML = HTML
-    })
+            document.getElementById("current").innerHTML = HTML
+        })
 }
-function getFiveDayForcast(city){
+function getFiveDayForcast(city) {
     //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
     var URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
     fetch(URL)
-    .then(response => response.json())
-    .then(apiResponse => {
-        console.log(apiResponse)
-      
-        let HTML =""
-        for(let i=0;i<apiResponse.list.length;i=i+8){
-        HTML += `<div class="card" style="width: 18rem;">
+        .then(response => response.json())
+        .then(apiResponse => {
+            console.log(apiResponse)
+
+            let HTML = ""
+            for (let i = 0; i < apiResponse.list.length; i = i + 8) {
+                HTML += `<div class="card" style="width: 18rem;">
         <div class="card-body">
         <h6 class="card-title">${apiResponse.list[i].dt_txt}</h6>
         <img src="https://openweathermap.org/img/wn/${apiResponse.list[i].weather[0].icon}@2x.png" class="card-img-top" alt="...">
@@ -57,7 +57,7 @@ function getFiveDayForcast(city){
           <p class="card-text">Description: ${apiResponse.list[i].weather[0].main}</p>
                 </div>
       </div>`
-        }
-      document.getElementById("fiveDay").innerHTML = HTML
-    })
+            }
+            document.getElementById("fiveDay").innerHTML = HTML
+        })
 }
